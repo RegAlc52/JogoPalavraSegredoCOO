c66e1ee270a9eb5faebacd25297f63021d4b345b
Regiane_de_Alcântara <any_any_2011@hotmail.com>
2024-03-03 09:24:38 -0300

    InicioProjeto

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+//Fazer um codigo para que o usuario adivinhe
+// Uma palavra secreta = Variaveis
+// terá 5 tentativas par adivinhar a palavra secreta = int
+// aparecer em tela os campos 
+// Digite a palavra secreta = String
+// Digite palpite = String
+// Informar se acertou a palavra secreta = Strig
+// Infomar se errou a palavra secreta, tentar novamente
+// Se errar todas as 5 tentativas, informar que acabaram as tentativas = int
+// Informar qual era a palavra secreta = String
+// Com orientação a objetos
+
+
+//Strings
+//Inserir a palavra secreta
+//Bem-vindo ao jogo de adivinhação
+//Tente adivinhar a palavra secreta
+//Digitar o seu palpite
+//Palavra incorreta, tente novamente
+//Parabéns você acertou a palavra secreta 
+//Suas tentativas entativas  acabaram
+
+//Variaceis
+//palavraSecreta
+//tentativasMaximas
+//tentativas
+//palpite
+
